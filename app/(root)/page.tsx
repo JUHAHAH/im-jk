a734cacf2613f6ae8fd06be4c104fdba0adcc5e0
JUHAHAH <juha0227@gmail.com>
2024-06-29 14:28:42 +0900

    Init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import { Portrait } from '@/components/api/Spline';
+
+const Home = () => {
+  return (
+    <div>
+      <div></div>
+      <div className="h-screen font-ndgm">
+        <Portrait />
+        asd
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ components/api/Spline.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline/next';
+
+export const Portrait = () => {
+  return (
+    <div>
+      <Spline scene="https://prod.spline.design/K4DB9Kg2XkwlerlE/scene.splinecode" />
+    </div>
+  );
+};
