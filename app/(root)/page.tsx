3175425407f7c4fdacdcf72673ed555edc75a110
JUHAHAH <juha0227@gmail.com>
2024-06-30 22:06:03 +0900

    asd

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,22 @@
+import { Portrait } from '@/components/api/Spline';
 import Main from '@/components/layouts/Main';
 import Portfolio from '@/components/layouts/Portfolio';
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="font-ndgm mb-4">
-        <Main />
-      </div>
+    <div className="flex flex-col items-center snap-mandatory snap-y overflow-scroll scroll-smooth">
+      <section className="h-screen font-ndgm mb-4 snap-start">
+        <div className="flex flex-col xl:flex-row">
+          <Portrait />
+          <Main />
+        </div>
+      </section>
 
-      <div className="">
+      <div className="snap-start">sss</div>
+
+      <section className="h-screen snap-start">
         <Portfolio />
-      </div>
+      </section>
     </div>
   );
 };
